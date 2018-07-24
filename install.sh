ea935b030a19c444a8ae775664206bda5bec7c42
Yusaku Hieda <y.hieda@chillda.com>
2018-07-24 11:50:58 +0900

    既存ファイルがあっても置き換えるよう修正

--- install.sh
+++ install.sh
@@ -1,11 +1,23 @@
 #!/bin/sh
 
 # neovim
+rm -rf ~/.config/nvim
 ln -sf ~/dotfiles/nvim ~/.config/nvim
+
 # vim
+rm -rf ~/.vim
 ln -sf ~/dotfiles/.vim ~/.vim
+
+rm -f ~/.vimrc
 ln -s ~/dotfiles/vimrc ~/.vimrc
+
+rm -f ~/.gvimrc
 ln -s ~/dotfiles/gvimrc ~/.gvimrc
+
+#tmux
+rm -f ~/.tmux.conf
+ln -s ~/dotfiles/.tmux/.tmux.conf ~/.tmux.conf
+
 # zsh
 # ln -s ~/dotfiles/zshrc ~/.zshrc
 
