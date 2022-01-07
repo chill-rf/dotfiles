8e70b94ca33b15e0f0d3f155c0e615e6d6a9df61
chill-rf <uiyiuyiyi@gmail.com>
2022-01-08 03:39:43 +0900

    coc.nvim復活

--- install_mac.sh
+++ install_mac.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 brew install neovim deno ghq go
 brew tap homebrew/cask-fonts
-brew install font-fira-code font-powerline-symbols
+brew install font-fira-code font-powerline-symbols font-hackgen font-hackgen-nerd
 
 git config --global ghq.root '~/git'
