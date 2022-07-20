bcd94f782feab2501905d1568cb762162affcf04
chill-rf <uiyiuyiyi@gmail.com>
2022-07-21 02:23:48 +0900

    edit shell

--- switch-nvim-lua.sh
+++ switch-nvim-lua.sh
@@ -2,6 +2,4 @@
 
 echo "switch init.lua"
 NEOVIM_DIR=~/.config/nvim
-unlink $NEOVIM_DIR
-rm -r $NEOVIM_DIR
-ln -s ~/dotfiles/nvim $NEOVIM_DIR
+ln -snf ~/dotfiles/nvim/ $NEOVIM_DIR
--- switch-nvim-old.sh
+++ switch-nvim-old.sh
@@ -2,6 +2,4 @@
 
 echo "switch init.vim"
 NEOVIM_DIR=~/.config/nvim
-unlink $NEOVIM_DIR
-rm -r $NEOVIM_DIR
-ln -s ~/dotfiles/nvim-old $NEOVIM_DIR
+ln -snf ~/dotfiles/nvim-old $NEOVIM_DIR
