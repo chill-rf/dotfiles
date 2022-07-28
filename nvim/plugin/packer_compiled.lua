4c68e484a53e59c029a4da29110834279f7a028e
chill-rf <uiyiuyiyi@gmail.com>
2022-07-29 03:17:14 +0900

    edit

--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -83,6 +83,11 @@ _G.packer_plugins = {
     path = "/Users/chill-rf/.local/share/nvim/site/pack/packer/opt/LuaSnip",
     url = "https://github.com/L3MON4D3/LuaSnip"
   },
+  ["barbar.nvim"] = {
+    loaded = true,
+    path = "/Users/chill-rf/.local/share/nvim/site/pack/packer/start/barbar.nvim",
+    url = "https://github.com/romgrk/barbar.nvim"
+  },
   ["cmp-buffer"] = {
     after_files = { "/Users/chill-rf/.local/share/nvim/site/pack/packer/opt/cmp-buffer/after/plugin/cmp_buffer.lua" },
     load_after = {
@@ -121,6 +126,17 @@ _G.packer_plugins = {
     path = "/Users/chill-rf/.local/share/nvim/site/pack/packer/opt/cmp-path",
     url = "https://github.com/hrsh7th/cmp-path"
   },
+  ["cmp-skkeleton"] = {
+    after_files = { "/Users/chill-rf/.local/share/nvim/site/pack/packer/opt/cmp-skkeleton/after/plugin/cmp_skkeleton.lua" },
+    load_after = {
+      ["nvim-cmp"] = true
+    },
+    loaded = false,
+    needs_bufread = false,
+    only_cond = false,
+    path = "/Users/chill-rf/.local/share/nvim/site/pack/packer/opt/cmp-skkeleton",
+    url = "https://github.com/rinx/cmp-skkeleton"
+  },
   cmp_luasnip = {
     after_files = { "/Users/chill-rf/.local/share/nvim/site/pack/packer/opt/cmp_luasnip/after/plugin/cmp_luasnip.lua" },
     load_after = {
@@ -131,11 +147,22 @@ _G.packer_plugins = {
     path = "/Users/chill-rf/.local/share/nvim/site/pack/packer/opt/cmp_luasnip",
     url = "https://github.com/saadparwaiz1/cmp_luasnip"
   },
+  ["denops.vim"] = {
+    loaded = true,
+    path = "/Users/chill-rf/.local/share/nvim/site/pack/packer/start/denops.vim",
+    url = "https://github.com/vim-denops/denops.vim"
+  },
   ["fidget.nvim"] = {
     config = { "\27LJ\2\n8\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\vfidget\frequire\0" },
     loaded = true,
     path = "/Users/chill-rf/.local/share/nvim/site/pack/packer/start/fidget.nvim",
     url = "https://github.com/j-hui/fidget.nvim"
+  },
+  ["gitsigns.nvim"] = {
+    config = { "\27LJ\2\n6\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\rgitsigns\frequire\0" },
+    loaded = true,
+    path = "/Users/chill-rf/.local/share/nvim/site/pack/packer/start/gitsigns.nvim",
+    url = "https://github.com/lewis6991/gitsigns.nvim"
   },
   ["lualine.nvim"] = {
     config = { "\27LJ\2\n4\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\25pluginconfig.lualine\frequire\0" },
@@ -167,6 +194,7 @@ _G.packer_plugins = {
   },
   ["nvim-autopairs"] = {
     after = { "nvim-cmp" },
+    config = { "\27LJ\2\n@\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\19nvim-autopairs\frequire\0" },
     loaded = false,
     needs_bufread = false,
     only_cond = false,
@@ -174,7 +202,7 @@ _G.packer_plugins = {
     url = "https://github.com/windwp/nvim-autopairs"
   },
   ["nvim-cmp"] = {
-    after = { "cmp-buffer", "cmp-cmdline", "cmp-path", "cmp_luasnip" },
+    after = { "cmp-buffer", "cmp-cmdline", "cmp-path", "cmp-skkeleton", "cmp_luasnip" },
     config = { "\27LJ\2\n5\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\26pluginconfig/nvim-cmp\frequire\0" },
     load_after = {
       LuaSnip = true,
@@ -184,6 +212,12 @@ _G.packer_plugins = {
     needs_bufread = false,
     path = "/Users/chill-rf/.local/share/nvim/site/pack/packer/opt/nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
+  },
+  ["nvim-colorizer.lua"] = {
+    config = { "\27LJ\2\n7\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\14colorizer\frequire\0" },
+    loaded = true,
+    path = "/Users/chill-rf/.local/share/nvim/site/pack/packer/start/nvim-colorizer.lua",
+    url = "https://github.com/norcalli/nvim-colorizer.lua"
   },
   ["nvim-lsp-installer"] = {
     config = { "\27LJ\2\n5\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\26pluginconfig.nvim-lsp\frequire\0" },
@@ -224,6 +258,20 @@ _G.packer_plugins = {
     path = "/Users/chill-rf/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
+  skkeleton = {
+    after = { "cmp-skkeleton" },
+    config = { "\27LJ\2\n6\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\27pluginconfig.skkeleton\frequire\0" },
+    loaded = true,
+    needs_bufread = false,
+    path = "/Users/chill-rf/.local/share/nvim/site/pack/packer/opt/skkeleton",
+    url = "https://github.com/vim-skk/skkeleton"
+  },
+  ["skkeleton_indicator.nvim"] = {
+    config = { "\27LJ\2\nÔ\3\0\0\4\0\a\0\r6\0\0\0009\0\1\0009\0\2\0'\2\3\0+\3\1\0B\0\3\0016\0\4\0'\2\5\0B\0\2\0029\0\6\0004\2\0\0B\0\2\1K\0\1\0\nsetup\24skkeleton_indicator\frequireè\2      hi SkkeletonIndicatorEiji guifg=#88c0d0 guibg=#2e3440 gui=bold\n      hi SkkeletonIndicatorHira guifg=#2e3440 guibg=#a3be8c gui=bold\n      hi SkkeletonIndicatorKata guifg=#2e3440 guibg=#ebcb8b gui=bold\n      hi SkkeletonIndicatorHankata guifg=#2e3440 guibg=#b48ead gui=bold\n      hi SkkeletonIndicatorZenkaku guifg=#2e3440 guibg=#88c0d0 gui=bold\n    \14nvim_exec\bapi\bvim\0" },
+    loaded = true,
+    path = "/Users/chill-rf/.local/share/nvim/site/pack/packer/start/skkeleton_indicator.nvim",
+    url = "https://github.com/delphinus/skkeleton_indicator.nvim"
+  },
   ["telescope.nvim"] = {
     config = { "\27LJ\2\n6\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\27pluginconfig.telescope\frequire\0" },
     load_after = {},
@@ -263,10 +311,37 @@ if not vim.g.packer_custom_loader_enabled then
   vim.g.packer_custom_loader_enabled = true
 end
 
+-- Setup for: skkeleton
+time([[Setup for skkeleton]], true)
+try_loadstring("\27LJ\2\n4\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\rcomplete\bcmp\frequireµ\1\0\0\5\1\v\0\0206\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\2.\0\0\0006\0\0\0'\2\3\0B\0\2\0029\0\4\0009\0\5\0005\2\a\0004\3\3\0005\4\6\0>\4\1\3=\3\b\0025\3\t\0=\3\n\2B\0\2\1K\0\1\0\0€\tview\1\0\1\fentries\vnative\fsources\1\0\0\1\0\1\tname\14skkeleton\vbuffer\nsetup\bcmp\bget\15cmp.config\frequireR\0\0\3\1\4\0\f-\0\0\0\15\0\0\0X\1\b€6\0\0\0'\2\1\0B\0\2\0029\0\2\0009\0\3\0-\2\0\0B\0\2\0011\0\0\0K\0\1\0\0€\vbuffer\nsetup\bcmp\frequireˆ\3\1\0\a\0\20\0)6\0\0\0009\0\1\0009\0\2\0005\2\3\0'\3\4\0'\4\5\0B\0\4\0016\0\0\0009\0\1\0009\0\2\0'\2\6\0'\3\a\0003\4\b\0B\0\4\1+\0\0\0006\1\0\0009\1\t\0019\1\n\1'\3\v\0004\4\0\0B\1\3\0026\2\0\0009\2\t\0029\2\f\2'\4\r\0005\5\14\0=\1\15\0053\6\16\0=\6\17\5B\2\3\0016\2\0\0009\2\t\0029\2\f\2'\4\r\0005\5\18\0=\1\15\0053\6\19\0=\6\17\5B\2\3\0012\0\0€K\0\1\0\0\1\0\1\fpattern\26skkeleton-disable-pre\rcallback\0\ngroup\1\0\1\fpattern\25skkeleton-enable-pre\tUser\24nvim_create_autocmd\24skkeleton_callbacks\24nvim_create_augroup\bapi\0\15<C-x><C-o>\6i\29<Plug>(skkeleton-enable)\n<C-j>\1\4\0\0\6i\6c\6l\bset\vkeymap\bvim\0", "setup", "skkeleton")
+time([[Setup for skkeleton]], false)
+time([[packadd for skkeleton]], true)
+vim.cmd [[packadd skkeleton]]
+time([[packadd for skkeleton]], false)
+-- Config for: skkeleton_indicator.nvim
+time([[Config for skkeleton_indicator.nvim]], true)
+try_loadstring("\27LJ\2\nÔ\3\0\0\4\0\a\0\r6\0\0\0009\0\1\0009\0\2\0'\2\3\0+\3\1\0B\0\3\0016\0\4\0'\2\5\0B\0\2\0029\0\6\0004\2\0\0B\0\2\1K\0\1\0\nsetup\24skkeleton_indicator\frequireè\2      hi SkkeletonIndicatorEiji guifg=#88c0d0 guibg=#2e3440 gui=bold\n      hi SkkeletonIndicatorHira guifg=#2e3440 guibg=#a3be8c gui=bold\n      hi SkkeletonIndicatorKata guifg=#2e3440 guibg=#ebcb8b gui=bold\n      hi SkkeletonIndicatorHankata guifg=#2e3440 guibg=#b48ead gui=bold\n      hi SkkeletonIndicatorZenkaku guifg=#2e3440 guibg=#88c0d0 gui=bold\n    \14nvim_exec\bapi\bvim\0", "config", "skkeleton_indicator.nvim")
+time([[Config for skkeleton_indicator.nvim]], false)
+-- Config for: skkeleton
+time([[Config for skkeleton]], true)
+try_loadstring("\27LJ\2\n6\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\27pluginconfig.skkeleton\frequire\0", "config", "skkeleton")
+time([[Config for skkeleton]], false)
+-- Config for: nvim-colorizer.lua
+time([[Config for nvim-colorizer.lua]], true)
+try_loadstring("\27LJ\2\n7\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\14colorizer\frequire\0", "config", "nvim-colorizer.lua")
+time([[Config for nvim-colorizer.lua]], false)
 -- Config for: fidget.nvim
 time([[Config for fidget.nvim]], true)
 try_loadstring("\27LJ\2\n8\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\vfidget\frequire\0", "config", "fidget.nvim")
 time([[Config for fidget.nvim]], false)
+-- Config for: nvim-lsp-installer
+time([[Config for nvim-lsp-installer]], true)
+try_loadstring("\27LJ\2\n5\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\26pluginconfig.nvim-lsp\frequire\0", "config", "nvim-lsp-installer")
+time([[Config for nvim-lsp-installer]], false)
+-- Config for: gitsigns.nvim
+time([[Config for gitsigns.nvim]], true)
+try_loadstring("\27LJ\2\n6\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\rgitsigns\frequire\0", "config", "gitsigns.nvim")
+time([[Config for gitsigns.nvim]], false)
 -- Config for: null-ls.nvim
 time([[Config for null-ls.nvim]], true)
 try_loadstring("\27LJ\2\n4\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\25pluginconfig/null-ls\frequire\0", "config", "null-ls.nvim")
@@ -279,10 +354,6 @@ time([[Config for Comment.nvim]], false)
 time([[Config for lualine.nvim]], true)
 try_loadstring("\27LJ\2\n4\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\25pluginconfig.lualine\frequire\0", "config", "lualine.nvim")
 time([[Config for lualine.nvim]], false)
--- Config for: nvim-lsp-installer
-time([[Config for nvim-lsp-installer]], true)
-try_loadstring("\27LJ\2\n5\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\26pluginconfig.nvim-lsp\frequire\0", "config", "nvim-lsp-installer")
-time([[Config for nvim-lsp-installer]], false)
 -- Config for: neo-tree.nvim
 time([[Config for neo-tree.nvim]], true)
 try_loadstring("\27LJ\2\n5\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\26pluginconfig.neo-tree\frequire\0", "config", "neo-tree.nvim")
@@ -290,22 +361,23 @@ time([[Config for neo-tree.nvim]], false)
 -- Load plugins in order defined by `after`
 time([[Sequenced loading]], true)
 vim.cmd [[ packadd nightfox.nvim ]]
-vim.cmd [[ packadd telescope.nvim ]]
-
--- Config for: telescope.nvim
-try_loadstring("\27LJ\2\n6\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\27pluginconfig.telescope\frequire\0", "config", "telescope.nvim")
-
 vim.cmd [[ packadd nvim-treesitter ]]
 
 -- Config for: nvim-treesitter
 try_loadstring("\27LJ\2\n<\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0!pluginconfig/nvim-treesitter\frequire\0", "config", "nvim-treesitter")
+
+vim.cmd [[ packadd telescope.nvim ]]
+
+-- Config for: telescope.nvim
+try_loadstring("\27LJ\2\n6\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\27pluginconfig.telescope\frequire\0", "config", "telescope.nvim")
 
 time([[Sequenced loading]], false)
 vim.cmd [[augroup packer_load_aucmds]]
 vim.cmd [[au!]]
   -- Event lazy-loads
 time([[Defining lazy-load event autocommands]], true)
-vim.cmd [[au VimEnter * ++once lua require("packer.load")({'LuaSnip', 'nvim-autopairs'}, { event = "VimEnter *" }, _G.packer_plugins)]]
+vim.cmd [[au VimEnter * ++once lua require("packer.load")({'nvim-autopairs', 'LuaSnip'}, { event = "VimEnter *" }, _G.packer_plugins)]]
+vim.cmd [[au InsertEnter * ++once lua require("packer.load")({'cmp-skkeleton'}, { event = "InsertEnter *" }, _G.packer_plugins)]]
 time([[Defining lazy-load event autocommands]], false)
 vim.cmd("augroup END")
 if should_profile then save_profiles() end
