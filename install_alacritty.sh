b3bd1db387727da39b2eef960ca307d0c67eab59
Yusaku Hieda <y.hieda@chill-rf.com>
2022-07-29 12:30:38 +0900

    edit

---
+++ install_alacritty.sh
@@ -0,0 +1,18 @@
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+rustup override set stable
+rustup update stable
+
+sudo apt install -y cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
+
+mkdir ~/build
+cd build
+git clone https://github.com/alacritty/alacritty.git
+cd alacritty
+cargo build --release
+
+sudo cp target/release/alacritty /usr/local/bin
+sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
+sudo desktop-file-install extra/linux/Alacritty.desktop
+sudo update-desktop-database
+
+cd ~/dotfiles
