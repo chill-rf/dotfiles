a8be833f3bb0b99f68efb25de0da2df6e745b82b
chill-rf <y.hieda@chill-rf.com>
2022-09-06 20:28:51 +0900

    fix

--- install_alacritty.sh
+++ install_alacritty.sh
@@ -1,4 +1,9 @@
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+#!/bin/bash
+
+if [ ! -e `${HOME}/.rustup` ]; then
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+fi
+
 rustup override set stable
 rustup update stable
 
@@ -6,12 +11,17 @@ sudo apt install -y cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-
 
 mkdir ~/build
 cd ~/build
+
+if [ -e `./alacritty` ]; then
+  rm -rf ./alacritty
+fi
+
 git clone https://github.com/alacritty/alacritty.git
 cd alacritty
 cargo build --release
 
-sudo cp target/release/alacritty /usr/local/bin
-sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
+sudo cp -f target/release/alacritty /usr/local/bin
+sudo cp -f extra/logo/compat/alacritty-term.png /usr/share/pixmaps/Alacritty.png
 sudo desktop-file-install extra/linux/Alacritty.desktop
 sudo update-desktop-database
 
