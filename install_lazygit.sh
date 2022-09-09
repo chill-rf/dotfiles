4880162d0ad39e5bccbfe63a3935f15ba8d70ccf
Yusaku Hieda <y.hieda@chill-rf.com>
2022-09-10 02:15:19 +0900

    edit

--- install_lazygit.sh
+++ install_lazygit.sh
@@ -1,4 +1,8 @@
 export VER="0.35"
+mkdir tmp_lazygit
+cd tmp_lazygit
 wget -O lazygit.tgz https://github.com/jesseduffield/lazygit/releases/download/v${VER}/lazygit_${VER}_Linux_arm64.tar.gz
 tar xvf lazygit.tgz
 sudo mv lazygit /usr/local/bin/
+cd ..
+rm -rf tmp_lazygit
