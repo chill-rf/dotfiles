3478141cf6c08347ba92ba1a586695b8f499c539
y.hieda@chill-rf.com <y.hieda@chill-rf.com>
2022-09-12 21:44:44 +0900

    neovim install update

--- install_neovim.sh
+++ install_neovim.sh
@@ -1,7 +1,14 @@
+#!/bin/bash
+
 sudo apt update
 sudo apt-get -y install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen libsqlite3-dev
 mkdir ~/build
 cd ~/build
+
+if [ -e `./neovim` ]; then
+  rm -rf ./neovim
+fi
+
 git clone https://github.com/neovim/neovim
 cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
 sudo make install
