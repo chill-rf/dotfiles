d06b225cacd166f695668199d2644afb3b1456f7
y.hieda@chill-rf.com <y.hieda@chill-rf.com>
2022-09-12 21:44:53 +0900

    chromebook install add

---
+++ install-chromebook.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+./install.sh
+
+# alacritty
+echo "alacritty for chromebook"
+ALACRITTY_DIR=~/.config/alacritty
+ALACRITTY_FILE=$ALACRITTY_DIR/alacritty.yml
+unlink $ALACRITTY_DIR
+rm -rf $ALACRITTY_DIR
+mkdir -p $ALACRITTY_DIR
+ln -sf ~/dotfiles/.config/alacritty/alacritty-chromebook.yml $ALACRITTY_FILE
+
+exit 0
