d432c7f424db1706c755c113eff10c1f64902849
Yusaku Hieda <y.hieda@chill-rf.com>
2022-09-24 02:49:52 +0900

    fix

--- install.sh
+++ install.sh
@@ -189,6 +189,11 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/${VAR_NVM}/install.sh | ba
 # pyenv
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ${HOME}/.bash_profile
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ${HOME}/.bash_profile
+source ${HOME}/.bash_profile
+eval "$(pyenv init -)"
+
 # pyenv-virtualenv
 git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
 
