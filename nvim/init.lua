f57580c0e9da6c28130eb36d2586c73813fda5b5
Yusaku Hieda <uiyiuyiyi@gmail.com>
2023-01-16 09:53:10 +0900

    vscodeの場合、プラグインを使用しないように修正

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,12 @@
-require("base")
 
-require("plugins")
-require("colorscheme")
+if vim.g.vscode then
+  -- VSCode extension
+  require("base-vscode")
+else
+  -- ordinary Neovim
+  require("base")
+  require("plugins")
+  require("colorscheme")
+end
+
 require("mapping")
