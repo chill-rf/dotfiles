4443f04d0d6e7841230df3bc84b009deddbf358d
Yusaku Hieda <uiyiuyiyi@gmail.com>
2023-01-19 16:03:43 +0900

    fix wezterm fontのデフォルト太字をやめる

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,7 +2,8 @@ local wezterm = require("wezterm")
 local act = wezterm.action
 
 return {
-	font = wezterm.font("HackGen Console NF", { weight = "Bold" }),
+	-- font = wezterm.font("HackGen Console NF", { weight = "Bold" }),
+	font = wezterm.font("HackGen Console NF"),
 	font_size = 16.0,
 	color_scheme = "nightfox",
 	window_background_opacity = 0.9,
