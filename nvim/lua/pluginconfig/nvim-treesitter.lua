e2ed2491ccd42b12238d3b8316d71e38ae9427ea
Yusaku Hieda <uiyiuyiyi@gmail.com>
2023-01-23 00:26:42 +0900

    fix

--- nvim/lua/pluginconfig/nvim-treesitter.lua
+++ nvim/lua/pluginconfig/nvim-treesitter.lua
@@ -1,12 +1,12 @@
 require("nvim-treesitter.configs").setup({
-  ensure_installed = "all", -- :TSInstall allと同じ
-  ignore_install = { "phpdoc", "tree-sitter-phpdoc" },
-  highlight = {
-    enable = true, -- syntax highlightを有効にする
-    disable = {},
-    additional_vim_regex_highlighting = false,
-  },
-  context_commentstring = { enable = true },
-  yati = { enable = true, disable = { "markdown" } },
-  tree_setter = { enable = true },
+	ensure_installed = "all", -- :TSInstall allと同じ
+	ignore_install = { "phpdoc", "tree-sitter-phpdoc" },
+	highlight = {
+		enable = true, -- syntax highlightを有効にする
+		disable = {},
+		additional_vim_regex_highlighting = false,
+	},
+	context_commentstring = { enable = true },
+	yati = { enable = true, disable = { "markdown" } },
+	tree_setter = { enable = true },
 })
