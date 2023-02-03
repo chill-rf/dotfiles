fb1896d3c38541d0eb75900b8adc09de56681bc3
Yusaku Hieda <y.hieda@chill-rf.com>
2023-02-04 01:03:16 +0900

    fix neovim git diffをdeltaで表示するように設定

--- install.sh
+++ install.sh
@@ -41,7 +41,13 @@ bash ${HOME}/dotfiles/bin/apps/setup.sh
 echo "apps setup finish."
 
 # git config
-git config --global user.name 'Yusaku Hieda' 
-git config --global user.email 'y.hieda@chill-rf.com' 
+git config --global user.name 'Yusaku Hieda'
+git config --global user.email 'y.hieda@chill-rf.com'
+git config --global core.editor 'nvim'
+
+git config --global core.pager 'delta'
+git config --global interactive.diffFilter 'delta --color-only'
+git config --global delta.navigate 'true'
+git config --global delta.light 'false'
 
 exit 0
