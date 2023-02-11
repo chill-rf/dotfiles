b51cb6ae7fce2bec8ba9ad724ea8c111de066def
Yusaku Hieda <uiyiuyiyi@gmail.com>
2023-02-12 03:33:14 +0900

    fix git メールアドレス変更

--- bin/config.sh
+++ bin/config.sh
@@ -2,7 +2,8 @@
 
 # git config
 git config --global user.name 'Yusaku Hieda'
-git config --global user.email 'y.hieda@chill-rf.com'
+# git config --global user.email 'y.hieda@chill-rf.com'
+git config --global user.email 'uiyiuyiyi@gmail.com'
 git config --global core.editor 'nvim'
 
 git config --global core.pager 'delta'
