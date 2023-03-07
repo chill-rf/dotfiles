dc6eb6e338f96b89f5a92be20b59b9b49f8376b3
Yusaku Hieda <uiyiuyiyi@gmail.com>
2023-03-07 22:37:33 +0900

    fix zsh complete

---
+++ .config/zsh/zshrc/Darwin/base.zsh
@@ -0,0 +1,2 @@
+complete -C '/opt/homebrew/bin/aws_completer' aws
+complete -o nospace -C /opt/homebrew/bin/terraform terraform
--- .config/zsh/zshrc/base.zsh
+++ .config/zsh/zshrc/base.zsh
@@ -4,6 +4,7 @@ SAVEHIST=10000
 
 # 補完機能を有効にする
 autoload -Uz compinit && compinit -u
+autoload bashcompinit && bashcompinit
 
 # 補完メニューをカーソルで選択可能にする
 zstyle ':completion:*:default' menu select=2
